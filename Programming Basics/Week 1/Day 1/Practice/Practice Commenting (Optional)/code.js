8aee96d55223a0c54060103035fb440667b974e5
DhiaBenHass1ne <dhiabhg@gmail.com>
2023-10-16 15:08:13 +0100

    Practice Commenting

---
+++ Programming Basics/Week 1/Day 1/Practice/Practice Commenting (Optional)/code.js
@@ -0,0 +1,7 @@
+// A variable that counts likes.
+var likeCount = 0;
+// Increases the like counter every time it's called.
+function increaseLikes() {
+    // increment the variable by 1.
+    likeCount = likeCount + 1;
+}
