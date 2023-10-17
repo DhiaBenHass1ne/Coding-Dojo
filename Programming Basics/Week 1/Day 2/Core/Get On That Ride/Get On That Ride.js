52294fd1b6b00581edd4cdd181d46155232f8a16
DhiaBenHass1ne <dhiabhg@gmail.com>
2023-10-17 11:23:53 +0100

    Get on that ride

---
+++ Programming Basics/Week 1/Day 2/Core/Get On That Ride/Get On That Ride.js
@@ -0,0 +1,10 @@
+function rideREQ (height, age){
+    if (height>=42 && age>=10) {
+        console.log("Get on that ride, kiddo!")
+    }
+    else{console.log("Sorry kiddo. Maybe next year.")}
+
+}
+var h=42;
+var a=9;
+rideREQ(h,a);
