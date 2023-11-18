a153ad97cfda82495cebc13c425bc735dcbb8fa3
DhiaBenHass1ne <dhiabhg@gmail.com>
2023-11-18 16:22:33 +0100

    Chaining methods

--- Python/Week 1/Day 2/Practice/Chaining Methods/user.py
+++ Python/Week 1/Day 2/Practice/Chaining Methods/user.py
@@ -10,6 +10,7 @@ class User:
     def display_info(self):
         for v in self.__dict__.items():
             print(':'.join([str(i) for i in v]))
+        return self
     
     def enroll(self):
         if self.is_reward_member:
@@ -18,13 +19,14 @@ class User:
         else:
             self.is_reward_member=True
             self.gold_card_points=200
+        return self
 
     def spend_points(self,amount):
         if self.gold_card_points<amount:
             print("Not enough cash! Stranger.")
         else:
             self.gold_card_points-=amount
-        
+        return self
 
 dhia=User('dhia','ben hassine',"dhfg@dgdg.sf",25)
 
@@ -37,12 +39,5 @@ dhia.enroll()
 user1=User("slimen","labyedh","slouma@gmail.com",53)
 user2=User("chedli","tamar","chdoula@gmail.com",56)
 
-user1.spend_points(50)
-
-user2.enroll()
-
-user2.spend_points(80)
-
-dhia.display_info()
-user1.display_info()
-user2.display_info()
+user1.display_info().enroll().spend_points(50).display_info()
+user2.enroll().spend_points(80).display_info()
