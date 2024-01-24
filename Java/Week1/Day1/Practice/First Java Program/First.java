584940d8b30078b65e40ad3d7c751840426d3380
DhiaBenHass1ne <dhiabhg@gmail.com>
2024-01-24 13:33:19 +0100

    First Java

---
+++ Java/Week1/Day1/Practice/First Java Program/First.java
@@ -0,0 +1,7 @@
+public class First {
+	public static void main(String[] args) {
+        System.out.println("Dhia");
+        System.out.println("25");
+        System.out.println("Bizerte");
+	}
+}
