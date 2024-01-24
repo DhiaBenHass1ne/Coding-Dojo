f4353b7b2df182474b52e5e2eeef478873c319e5
DhiaBenHass1ne <dhiabhg@gmail.com>
2024-01-24 13:26:01 +0100

    Java

--- MERNStack/week_3/Day_1/Core/Authors/client/src/App.jsx
+++ MERNStack/week_3/Day_1/Core/Authors/client/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import Home from './components/Home'
 import Update from './components/Update'
 import Create from './components/Create'
-
+import { DeepChat } from "deep-chat-react";
 function App() {
 
   return (
@@ -13,6 +13,7 @@ function App() {
       <Route path='/create' element={<Create/>} ></Route>
       <Route path='/author/update/:id' element={<Update/>} ></Route>
     </Routes>
+    <DeepChat ></DeepChat>
     </>
   )
 }
