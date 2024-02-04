3b23018b8a99bb58839c01b8353ed9bf1a87698b
DhiaBenHass1ne <dhiabhg@gmail.com>
2024-02-04 14:12:41 +0100

    Daikichi Routes

---
+++ Java/Week1/Day5/Practice/DaikichiRoutes/src/test/java/com/dhia/daikachiroutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhia.daikachiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
